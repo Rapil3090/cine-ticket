976b819677db7d67fa1f05718847f47235e0f7cb
RapiL <sjw3090gmail.com>
2023-10-05 15:07:18 +0900

    user 정보 검색을 위한 모듈 추가

--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -3,10 +3,14 @@ import { ReservationService } from './reservation.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ReservationController } from './reservation.controller';
 import { ReservationRepository } from './reservation.repository';
+import { UserModule } from '../user/user.module';
+import { UserRepository } from '../user/user.repository';
+import { MoviesModule } from '../movies/movies.module';
+import { MovieRepository } from '../movies/movies.repository';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule, MoviesModule],
   controllers: [ReservationController],
-  providers: [PrismaModule, ReservationService, ReservationRepository]
+  providers: [PrismaModule, ReservationService, ReservationRepository, UserRepository, MovieRepository]
 })
 export class ReservationModule {}
